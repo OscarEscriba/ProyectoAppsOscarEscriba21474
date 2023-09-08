4283e2f82c8e12dc06debdf30f2c6e767e0969e9
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2023-09-07 21:51:51 -0600

    agregar el cambio de pantalla en los layouts

--- app/src/main/java/com/example/proyectoappsoscarescrib21474/pantalla1.kt
+++ app/src/main/java/com/example/proyectoappsoscarescrib21474/pantalla1.kt
@@ -2,20 +2,28 @@ package com.example.proyectoappsoscarescrib21474
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.example.proyectoappsoscarescrib21474.databinding.ActivityMainBinding
+import com.example.proyectoappsoscarescrib21474.databinding.Pantalla1Binding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class pantalla1: AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+
+class Pantalla1: AppCompatActivity() {
+    private lateinit var binding: Pantalla1Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = Pantalla1Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //los eventos onclik para ir cambiando   entre cada una de las siguientes partes del menu...
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+        }
     }
     private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
 
