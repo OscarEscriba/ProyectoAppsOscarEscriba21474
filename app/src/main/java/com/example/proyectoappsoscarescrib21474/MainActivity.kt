ea0ab0c9f114236e0b6c1f7840305c992fcb8c6f
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2023-09-07 23:46:40 -0600

    ultimo commit

--- app/src/main/java/com/example/proyectoappsoscarescrib21474/MainActivity.kt
+++ app/src/main/java/com/example/proyectoappsoscarescrib21474/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.proyectoappsoscarescrib21474
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //codigo para darle funcionalidad al boton...
+        val Pantalla: Button = findViewById(R.id.button)
+        Pantalla.setOnClickListener{
+            openActivityPantalla()
+        }
+    }
+    private fun openActivityPantalla() {
+        //crear la intencion para abrir Activity
+        val intent = Intent(this, Pantalla1::class.java)
+        //iniciar la actividad
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/proyectoappsoscarescrib21474/Pantalla1.kt
+++ app/src/main/java/com/example/proyectoappsoscarescrib21474/Pantalla1.kt
@@ -1,20 +1,19 @@
 package com.example.proyectoappsoscarescrib21474
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.example.proyectoappsoscarescrib21474.databinding.Pantalla1Binding
+import com.example.proyectoappsoscarescrib21474.databinding.ActivityPantalla1Binding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
-class Pantalla1: AppCompatActivity() {
-    private lateinit var binding: Pantalla1Binding
+class Pantalla1 : AppCompatActivity() {
+    private lateinit var binding: ActivityPantalla1Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = Pantalla1Binding.inflate(layoutInflater)
+        binding = ActivityPantalla1Binding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Home())
-
         //los eventos onclik para ir cambiando   entre cada una de las siguientes partes del menu...
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId) {
@@ -34,4 +33,3 @@ class Pantalla1: AppCompatActivity() {
         fragmentTransaction.commit()
     }
 }
-
